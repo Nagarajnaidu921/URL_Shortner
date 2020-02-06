73601db2e0b8e81954aeb3af7e2ad8f2723bd2d7
Nagaraj Vemula <nagarajvemula96@gmail.com>
2020-02-06 18:47:41 +0530

    replaced g+ with linkedin and implemented fetch api

--- js/index.js
+++ js/index.js
@@ -1,53 +1,51 @@
- document.addEventListener('DOMContentLoaded', function() {
-     function urlGen(url) {
+document.addEventListener('DOMContentLoaded', function () {
+    async function urlGen(url) {
 
-         var xhttp = new XMLHttpRequest();
-         xhttp.onreadystatechange = function() {
-             if (this.readyState == 4 && this.status == 200) {
-                 document.getElementById('link').innerText = this.responseText;
-                 // document.getElementById('link').innerText = url;
+        try {
 
-                 document.getElementById('copy').style.display = 'block';
-                 document.getElementById('social-sharing-link-container-layout').style.display = 'block';
-                 new QRCode(document.getElementById('qrcode'), {
-                     text: this.responseText,
-                     width: 250,
-                     height: 250,
-                     correctLevel: QRCode.CorrectLevel.H
-                 });
-                 document.getElementById('facebook').href = 'https://www.facebook.com/sharer.php?u=' + this.responseText;
-                 document.getElementById('twitter').href = 'https://twitter.com/intent/tweet?url=' + this.responseText;
-                 document.getElementById('google-plus').href = 'https://plus.google.com/share?url=' + this.responseText;
-                 window.setTimeout( ()=>{
-                    document.getElementById('loader').style.display = 'none'
-                 }, 500);
-             }
-         };
-         xhttp.open('GET', url, true);
-         xhttp.send();
+            const res = await fetch(url);
+            const tinyUrl = await res.text();
+            document.getElementById('loader').style.display = 'none'
+            document.getElementById('link').innerText = tinyUrl;
+            document.getElementById('copy').style.display = 'block';
+            document.getElementById('social-sharing-link-container-layout').style.display = 'block';
+            new QRCode(document.getElementById('qrcode'), {
+                text: tinyUrl,
+                width: 250,
+                height: 250,
+                correctLevel: QRCode.CorrectLevel.H
+            });
+            document.getElementById('facebook').href = `https://www.facebook.com/sharer.php?u=${tinyUrl}`;
+            document.getElementById('twitter').href = `https://twitter.com/intent/tweet?url=${tinyUrl}`;
+            document.getElementById('linkedin').href = `https://www.linkedin.com/shareArticle?url=${tinyUrl}`;
+            setTimeout(() => {
+                document.getElementById('loader').style.display = 'none'
+            }, 500);
+        } catch (error) {
 
+        }
 
-         function copy(event) {
-             var range = document.createRange();
-             range.selectNode(document.getElementById('link'));
-             window.getSelection().addRange(range);
-             document.execCommand('Copy');
-         }
-         document.querySelector('#copy').addEventListener('click', copy);
+        function copy(event) {
+            var range = document.createRange();
+            range.selectNode(document.getElementById('link'));
+            window.getSelection().addRange(range);
+            document.execCommand('Copy');
+        }
+        document.querySelector('#copy').addEventListener('click', copy);
 
-     }
+    }
 
 
 
-     (function getCurrentTabUrl() {
-         let queryInfo = {
-             active: true,
-             currentWindow: true
-         };
-         chrome.tabs.query(queryInfo, function(tabs) {
-             let tab = tabs[0];
-             let tabUrL = 'http://tinyurl.com/api-create.php?url=' + tab.url;
-             urlGen(tabUrL)
-         });
-     })();
- });
+    (function getCurrentTabUrl() {
+        let queryInfo = {
+            active: true,
+            currentWindow: true
+        };
+        chrome.tabs.query(queryInfo, function (tabs) {
+            let tab = tabs[0];
+            let tabUrL = 'http://tinyurl.com/api-create.php?url=' + tab.url;
+            urlGen(tabUrL)
+        });
+    })();
+});
