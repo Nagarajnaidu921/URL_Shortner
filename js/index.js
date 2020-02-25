7d4b042068aaeba23e507be900ca39b7c557bfe9
Nagaraj Vemula <nagarajnaidu921@gmail.com>
2020-02-25 09:29:00 +0530

    bug fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+
     async function urlGen(url) {
         const loader = document.getElementById('loader');
         const link = document.getElementById('link');
         const copyEle = document.getElementById('copy');
         const socialSharingLinkContainer = document.getElementById('social-sharing-link-container-layout');
         try {
-
+            // urlShortene
             const res = await fetch(url);
             const tinyUrl = await res.text();
             loader.style.display = 'none'
@@ -40,15 +40,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    (function getCurrentTabUrl() {
-        let queryInfo = {
-            active: true,
-            currentWindow: true
-        };
-        chrome.tabs.query(queryInfo, function (tabs) {
-            let tab = tabs[0];
-            let tabUrL = 'http://tinyurl.com/api-create.php?url=' + tab.url;
-            urlGen(tabUrL)
-        });
-    })();
-});
+
+function onWindowLoad() {
+    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+        var tablink = tabs[0].url;
+        urlGen("https://tinyurl.com/api-create.php?url=" + encodeURIComponent(tablink))
+    });
+
+}
+
+
+window.onload = onWindowLoad;
